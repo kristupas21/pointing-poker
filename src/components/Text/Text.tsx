32ef24e40124e279e49fc21f957dba8f2e6e1240
Kristupas Rybakovas <kristupas.rybakovas@devbridge.com>
2021-01-24 21:05:28 +0200

    Remove invalid prop

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -5,20 +5,19 @@ import withText, { WithText } from './withText';
 interface Props extends WithText {
   className?: string;
   id: MessageId;
-  locale?: string;
   values?: Record<string, string>;
   tag?: keyof HTMLElementTagNameMap;
 }
 
 const Text: React.FC<Props> = (props) => {
-  const { className, id, locale = 'en', values, tag, getText } = props;
+  const { className, id, values, tag, getText } = props;
   const Tag = tag ? ({ children: c, className: cl }) =>
     React.createElement(tag, { className: cl }, c) : React.Fragment;
   const tagProps = tag && { className };
 
   return (
     <Tag {...tagProps}>
-      {getText(id, values, locale)}
+      {getText(id, values)}
     </Tag>
   );
 };
