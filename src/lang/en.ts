5ca254cf865ab6f8bf9d06d214f4094c4efd1444
Kristupas Rybakovas <kristupas.rybakovas@devbridge.com>
2021-01-25 18:36:09 +0200

    Add more translations

--- src/lang/en.ts
+++ src/lang/en.ts
@@ -7,14 +7,21 @@ export const messages = {
   'form.validation.min': 'Min {chars} characters',
   'form.validation.max': 'Max {chars} characters',
 
-  'session.id': 'Session Id',
-  'session.not-found': 'Session with id {id} not found',
+  'session.id': 'Session ID',
+  'session.not-found': 'Session with ID {id} not found',
   'session.start': 'Start Session',
   'session.join': 'Join session',
+
   'session.field.name.label': 'Name',
   'session.field.role.label': 'Role',
   'session.field.role.placeholder': 'Select a role',
   'session.field.observer.label': 'Join as observer',
+
+  'session.action.showVotes': 'Show votes',
+  'session.action.clearVotes': 'Clear votes',
+  'session.action.choosePoints': 'Choose points',
+  'session.action.useRoles': 'Use roles',
+  'session.action.pointValues': 'Point values',
 
   'user.role.backend': 'Backend Dev',
   'user.role.frontend': 'Frontend Dev',
